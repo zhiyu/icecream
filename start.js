5679339ba79a89b84446e71f3c4d75f262a3b30a
zhiyu <zhengzhiyu@yeah.net>
2012-07-05 23:16:55 +0800

    update

--- start.js
+++ start.js
@@ -2,24 +2,6 @@ var icecream   = require('./lib/icecream');
 
 var app = icecream.createServer();
 app.listen(3000);
-// app.use(express.bodyParser());
-// app.use(express.cookieParser('secret'));
-// app.use(
-// 	express.session({
-// 		secret: 'secret'
-// 	})
-// );
-
 // app.engine('.html', require('ejs').__express);
 // app.set('views', __dirname + '/app/views');
 // app.set('view engine', 'html');
-
-// app.get('*', function(req, res){
-//     nodee.dispatch(req,res);
-// });
-
-// app.post('*', function(req, res){
-//     nodee.dispatch(req,res);
-// });
-
-// app.listen(3000);
