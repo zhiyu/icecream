3e09fc2b53811e4afc4b8892f00a31b35e00b556
zhiyu <zhengzhiyu@yeah.net>
2012-07-08 22:51:03 +0800

    update

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -1,6 +1,6 @@
 var c = module.exports;
 
 c.index = function(req,res){
-        console.log("dd:"+req.session.user);
-        res.render('page/index',{});
+    console.log("dd:"+req.session.user);
+    res.render('page/index',{});
 }
--- start.js
+++ start.js
@@ -3,7 +3,7 @@ var icecream   = require('./lib/icecream');
 
 icecream.createServer();
 icecream.use(connect.cookieParser());
-icecream.use(connect.session({ secret:'my secret here'}));
+icecream.use(connect.session({ secret:'DFJLK8DFGJ933JKLFGJ2'}));
 icecream.listen(3000);
 
 icecream.set('defaultEngine', 'jade');
