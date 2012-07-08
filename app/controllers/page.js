3925b9197681d110e96e23af34a4b471e59c33a6
zhiyu <zhengzhiyu@yeah.net>
2012-07-08 22:41:25 +0800

    update

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -2,13 +2,5 @@ var c = module.exports;
 
 c.index = function(req,res){
         console.log("dd:"+req.session.user);
-        res.render('page/index',this.data);
-}
-
-c.test = function(req,res){
-    res.send("test1");
-}
-
-c.welcome2 = function(req,res){
-    res.send("test2dd22");
+        res.render('page/index',{});
 }
