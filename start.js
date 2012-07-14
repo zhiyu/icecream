99f183ce39ffe1347e6991fb913f1d6fbf732c89
zhiyu <zhengzhiyu@yeah.net>
2012-07-14 22:38:01 +0800

    update

--- start.js
+++ start.js
@@ -1,13 +1,6 @@
-var connect    = require('connect')
 var icecream   = require('./lib/icecream');
 
 icecream.createServer();
-icecream.use(connect.query());
-icecream.use(connect.bodyParser());
-icecream.use(connect.cookieParser());
-icecream.use(connect.session({ secret:'DFJLK8DFGJ933JKLFGJ2'}));
-icecream.listen(3000);
-
-icecream.set('defaultEngine', 'jade');
-icecream.set('appRoot',  __dirname +'/app');
 icecream.set('debug', true);
+
+icecream.listen(3000);
