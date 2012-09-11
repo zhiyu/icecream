7cbab76088371613bd21805673a93f7212d9c053
zhiyu <zhengzhiyu@yeah.net>
2012-09-11 10:46:11 +0800

    update

--- examples/simple/start.js
+++ examples/simple/start.js
@@ -4,7 +4,7 @@ var icecream   = require('../../src/lib/icecream');
 icecream.createServer();
 
 icecream.set('appRoot',  __dirname +'/app');
-icecream.set('debug', false);
+icecream.set('debug', true);
 
 icecream.use(connect.session({ secret:'DFJLK8DFGJ933JKLFGJ2'}));
 
