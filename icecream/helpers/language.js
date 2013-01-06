0ac2c77ac93987b2b8a0889862e807ce72d3db0e
zhiyu <zhengzhiyu@yeah.net>
2013-01-06 20:36:23 +0800

    update

--- icecream/helpers/language.js
+++ icecream/helpers/language.js
@@ -2,7 +2,7 @@ var Language = module.exports = {}
 
 Language.lang = function(key, session){
 	var languages = {};
-	if(session["lang"]){
+	if(session && session["lang"]){
         languages = icecream.getObject("languages", session["lang"]);
 	}else{
 		languages = icecream.getObject("languages", icecream.get("defaultLanguage"));
