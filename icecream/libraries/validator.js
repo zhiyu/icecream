ad6f3cad7d2aa6ec9b30fdb04458398cb4b61767
zhiyu <zhengzhiyu@yeah.net>
2013-11-06 15:49:06 +0800

    update

--- icecream/libraries/validator.js
+++ icecream/libraries/validator.js
@@ -83,7 +83,7 @@ prototype.isIP = function(){
 
 prototype.isMobile = function(){
     return this.match(function(object){
-        return object.search(/^1[3|4|5|8][0-9]\d{4,8}$/)==-1?false:true;
+        return object.search(/^1[3|4|5|8][0-9]\d{8}$/)==-1?false:true;
     });
 }
 
